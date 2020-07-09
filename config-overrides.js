14fcbc63d350613f76aa7a544f547f4b0a1bc10e
wangwanjun <wangwj@ui-tech.com.cn>
2020-07-09 21:28:13 +0800

    引入antd

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,20 @@
 
 
-const {override,addLessLoader} = require('customize-cra')
+const {override,addLessLoader,fixBabelImports} = require('customize-cra')
+const modifyVars = './lessVars.js'
 
 module.exports=override(
   addLessLoader({
     lessOptions: {
       javascriptEnabled: true,
+      modifyVars,
     }
-  })
+  }),
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    // style: 'css',
+    style: true,
+  }),
 
 )
--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import './index.less'
 
+import {Button} from 'antd'
+
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        App
+        <Button type='primary'>测试按钮</Button>
       </div>
     )
   }
